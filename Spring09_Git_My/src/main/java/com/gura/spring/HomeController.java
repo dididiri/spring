a3aa63f68b0172a5018f4c35652c4a26868aa45d
kangmin <dididiri1@naver.com>
2017-01-20 17:05:29 +0900

    마지막 기능 추가

--- Spring09_Git_My/src/main/java/com/gura/spring/HomeController.java
+++ Spring09_Git_My/src/main/java/com/gura/spring/HomeController.java
@@ -9,7 +9,7 @@ public class HomeController {
 	// "/home.do" 요청 처리 
 	@RequestMapping("/home") // 클라이언트의 요청 맵핑하기 위한 어노테이션
 	public String home() {
-		/*
+		/*   
 		 *  리턴되는 data typa 과 메소드명은 상황에 맞게 구성할수 있다.
 		 *  
 		 *  - 리턴 Type 을 String 으로 한다는 것의 의미
