f6fbed4ec78f410c02bfc614deac1b26d18191da
kangmin <dididiri1@naver.com>
2017-01-25 15:20:42 +0900

    암호화된 비밀번호로 로그인가능 구현

--- Spring09_Git_My/src/main/java/com/gura/spring/service/UsersServiceImpl.java
+++ Spring09_Git_My/src/main/java/com/gura/spring/service/UsersServiceImpl.java
@@ -31,10 +31,22 @@ public class UsersServiceImpl implements UsersService{
 		usersDao.insert(dto);
 	}
     //암호화된거 비교해야되고
+	/*
+	 * 아이디만 셀렉트한다 그럼 암호화된 비밀번호도 가져와서 비교한다
+	 */
 	@Override
 	public boolean isValid(UsersDto dto) {
+		//아이디가 유효한지 여부
+		boolean isValid=false;
 		
-		return usersDao.isValid(dto);
+		//아이디에 해당하는 DB에 저장된 암호회된 비밀번호를 읽어온다.
+		String password=usersDao.getPassword(dto.getId());
+		if(password != null){ // 아이디가 일단 존재하는 경우
+			//.maches(사용자가 입력한비 밀번호,DB 에 저장된 암호화 비밀번호)
+			// 를 이용해서 비밀번호가 맞는지 여부를 boolean type 으로 리턴받기
+			isValid=pEncoder.matches(dto.getPwd(), password);		
+		}
+		return isValid;
 	}
     //암호회된거 업데이트해야됨
 	@Override
